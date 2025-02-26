78d4504988956d8395e4a28df8896fcb79880d4a
kylych-dev <mirbekov1kylych@gmail.com>
2025-02-26 16:11:16 +0600

    set

--- Procfile
+++ Procfile
@@ -1,3 +1,2 @@
 web: gunicorn backend.core.wsgi --log-file -
-#or works good with external database
 web: python backend.manage.py migrate && gunicorn backend.core.wsgi
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-ffvm=39=%f-7fum#i72dkq4i4qt=@-u*e2jae1szp-1a9#pnqx
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['booksphere-production.up.railway.app','127.0.0.1','localhost','0.0.0.0']
+ALLOWED_HOSTS = ['*','booksphere-production.up.railway.app','127.0.0.1','localhost','0.0.0.0']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
