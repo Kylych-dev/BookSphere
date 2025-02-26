697562aa4f0b9d125238e6604eca1fd3dea9964d
kylych-dev <mirbekov1kylych@gmail.com>
2025-02-26 14:59:20 +0600

    diagram

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     'django_filters',
     'corsheaders',
     'decouple',
+    'django_extensions',
 
     'apps.users.apps.UsersConfig',
     'apps.library.apps.LibraryConfig'
